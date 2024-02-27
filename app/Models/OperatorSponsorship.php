9ac6fdcacb682c516bbe2b6219285f4a76e2b142
MarcoPercolla <percollamarco@virgilio.it>
2024-02-27 16:09:36 +0100

    review seeder

--- app/Models/OperatorSponsorship.php
+++ app/Models/OperatorSponsorship.php
@@ -8,14 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class OperatorSponsorship extends Model
 {
     use HasFactory;
-    public function sponsorship(){
-        return
-        $this->belongsToMany(Sponsorship::class);
 
+    protected $fillable = ['operator_id', 'sponsorship_id', 'start_date', 'end_date'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($operatorSponsorship) {
+            $sponsorship = $operatorSponsorship->sponsorship;
+
+            if ($sponsorship) {
+                $endDate = $operatorSponsorship->start_date->addDays($sponsorship->duration);
+                $operatorSponsorship->end_date = $endDate;
+            }
+        });
     }
-    public function operator(){
-        return
-        $this->belongsToMany(Operator::class);
+    public function sponsorship()
+    {
+        return $this->belongsToMany(Sponsorship::class)->using(OperatorSponsorship::class);
+    }
 
+    public function operator()
+    {
+        return $this->belongsToMany(Operator::class)->using(OperatorSponsorship::class);
     }
 }
