4176b4101f52764c724d078ece1b6f0cf70c27b4
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-02-27 16:08:38 +0100

    seed messaggi popolato realisticamente

--- resources/views/layouts/Homepage.blade.php
+++ resources/views/layouts/Homepage.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<h1 class="display-5 fw-bold">
-    Welcome to Laravel+Bootstrap 5
-</h1>
 
 <div>@foreach ($operators as $operator)
     <h2>{{$operator -> name}}</h2>
