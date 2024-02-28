855b5e88ab6daab8994d5dd78db1aa3d466bd364
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-02-28 02:53:48 +0100

    Crud Create Store OperatorSponsorship

--- app/Http/Requests/StoreOperatorSponsorshipRequest.php
+++ app/Http/Requests/StoreOperatorSponsorshipRequest.php
@@ -11,7 +11,7 @@ class StoreOperatorSponsorshipRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
 use App\Http\Controllers\OperatorController;
+use App\Http\Controllers\OperatorSponsorshipsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,7 @@ Route::middleware(['auth'])
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('operators', OperatorController::class);
+        Route::resource("operator-sponsorships", OperatorSponsorshipsController::class);
 
        
     });
