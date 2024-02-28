54419d386ee0d22cc880d037c1c956d836afc0af
kristianfiliposki <krisplay@outlook.it>
2024-02-28 12:13:21 +0100

    img for operators

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
 use App\Http\Controllers\OperatorController;
+use App\Http\Controllers\PaymentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +32,8 @@ Route::middleware(['auth'])
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('operators', OperatorController::class);
+        Route::get('/payment', PaymentController::class);
+
 
        
     });
