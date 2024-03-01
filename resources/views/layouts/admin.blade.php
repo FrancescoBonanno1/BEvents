70cddb27556ea70bd74171ac55c0c97a7325bbe7
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-03-01 01:38:35 +0100

    Card personale in dashboard (2)

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,31 +1,23 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
         integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
         crossorigin='anonymous' referrerpolicy='no-referrer' />
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
-
 <body>
     <div id="app">
-
         <div class="container-fluid vh-100">
             <div class="row h-100">
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
@@ -37,7 +29,7 @@
                                     <i class="fa-solid fa-home-alt fa-lg fa-fw"></i> Home
                                 </a>
                             </li>
-                
+
                             <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
                                     href="{{ route('admin.dashboard') }}">
@@ -45,8 +37,8 @@
                                 </a>
                             </li>
 
-                            
-                
+
+
                             <!-- <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('admin.operators.create') }}">
                                     <i class="fa-solid fa-plus fa-lg fa-fw"></i> Aggiungi operatore
@@ -61,7 +53,7 @@
                                 </li>
                             @else
                                 <li class="nav-item">
-                                    <a class="nav-link text-white" href="{{ route('admin.operators.edit', $operator[0]->id) }}">
+                                    <a class="nav-link text-white" href="{{ route('admin.operators.edit', $operator_id[0]->id) }}">
                                         <i class="fa-solid fa-plus fa-lg fa-fw"></i> Modifica operatore
                                     </a>
                                 </li>
@@ -74,7 +66,7 @@
                                     </a>
                                 </li>
                             @endif
-                
+
                             <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('logout') }}"
                                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
@@ -95,8 +87,6 @@
                 </main>
             </div>
         </div>
-
     </div>
 </body>
-
 </html>
