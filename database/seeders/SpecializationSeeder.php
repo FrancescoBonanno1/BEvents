0da87eecd3dfca7c49a2220b6d4bbac9e22bff64
MarcoPercolla <percollamarco@virgilio.it>
2024-03-03 03:17:30 +0100

    aggiusta seeder img specialization e link connessi

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -13,31 +13,31 @@ class Specializationseeder extends Seeder
      */
     public function run(): void
     {
-        $specializations = [ 
-            ['name' => 'Pianoforte', 'background_image' => '../img/specializzazioni/IMG_1189.jpg'],
-            ['name' => 'Chitarra', 'background_image' => '../img/specializzazioni/IMG_1190.jpg '],
-            ['name' => 'Violino', 'background_image' => ' ../img/specializzazioni/IMG_1191.jpg'],
-            ['name' => 'Canto', 'background_image' => ' ../img/specializzazioni/IMG_1192.jpg'],
-            ['name' => 'Batteria', 'background_image' => ' ../img/specializzazioni/IMG_1194.jpg'],
-            ['name' => 'Musicista', 'background_image' => ' ../img/specializzazioni/IMG_1195.jpg'],
-            ['name' => 'Fonico', 'background_image' => ' ../img/specializzazioni/IMG_1196.jpg'],
-            ['name' => 'Produttore', 'background_image' => ' ../img/specializzazioni/IMG_1197.jpg'],
-            ['name' => 'Tecnico del suono', 'background_image' => ' ../img/specializzazioni/IMG_1198.jpg'],
-            ['name' => 'Tecnico Luci', 'background_image' => ' ../img/specializzazioni/IMG_1199.jpg'],
-            ['name' => 'Tastierista', 'background_image' => ' ../img/specializzazioni/IMG_1200.jpg'],
-            ['name' => 'Violoncellista', 'background_image' => ' ../img/specializzazioni/IMG_1201.jpg'],
-            ['name' => 'Saxofonista', 'background_image' => ' ../img/specializzazioni/IMG_1202.jpg'],
-            ['name' => 'Trombettista', 'background_image' => ' ../img/specializzazioni/IMG_1203.jpg'],
-            ['name' => 'DJ', 'background_image' => ' ../img/specializzazioni/IMG_1206.jpg'],
-            
+        $specializations = [
+            ['name' => 'Pianoforte', 'background_image' => 'IMG_1189.jpg'],
+            ['name' => 'Chitarra', 'background_image' => 'IMG_1190.jpg '],
+            ['name' => 'Violino', 'background_image' => 'IMG_1191.jpg'],
+            ['name' => 'Canto', 'background_image' => 'IMG_1192.jpg'],
+            ['name' => 'Batteria', 'background_image' => 'IMG_1194.jpg'],
+            ['name' => 'Musicista', 'background_image' => 'IMG_1195.jpg'],
+            ['name' => 'Fonico', 'background_image' => 'IMG_1196.jpg'],
+            ['name' => 'Produttore', 'background_image' => 'IMG_1197.jpg'],
+            ['name' => 'Tecnico del suono', 'background_image' => 'IMG_1198.jpg'],
+            ['name' => 'Tecnico Luci', 'background_image' => 'IMG_1199.jpg'],
+            ['name' => 'Tastierista', 'background_image' => 'IMG_1200.jpg'],
+            ['name' => 'Violoncellista', 'background_image' => 'IMG_1201.jpg'],
+            ['name' => 'Saxofonista', 'background_image' => 'IMG_1202.jpg'],
+            ['name' => 'Trombettista', 'background_image' => 'IMG_1203.jpg'],
+            ['name' => 'DJ', 'background_image' => 'IMG_1206.jpg'],
+
         ];
 
         foreach ($specializations as $specialization) {
-            $newspecialization= new Specialization;
-            $newspecialization->name=$specialization['name'];
-            $newspecialization->background_image=$specialization['background_image'];
+            $newspecialization = new Specialization;
+            $newspecialization->name = $specialization['name'];
+            $newspecialization->background_image = $specialization['background_image'];
 
             $newspecialization->save();
         }
     }
-    }
+}
