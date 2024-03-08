3ab1a25f43d750415021df6baefc74331cd3d9a2
ReDMallard <marco@titori.it>
2024-03-08 12:14:17 +0100

    payment done

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\MyMessageController;
 use App\Http\Controllers\MyReviewController;
 use App\Http\Controllers\OperatorController;
 use App\Http\Controllers\OperatorSponsorshipsController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ReviewController;
 
 /*
@@ -24,6 +25,14 @@ use App\Http\Controllers\ReviewController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pay', function () {
+    return view('payment_form');
+});
+
+Route::get('/checkout', [PaymentController::class, 'showPaymentPage'])->name('checkout');
+Route::get('/payment-form', [PaymentController::class, 'showPaymentPage'])->name('payment-form.show');
+Route::get('/client-token', [PaymentController::class, 'getClientToken']);
+Route::post('/process-payment', [PaymentController::class, 'processPayment'])->name('process-payment');
 
 Route::get('/dashboard', [OperatorController::class, 'index'])->name('dashboard');
 
@@ -31,31 +40,14 @@ Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () 
 
     // Dashboard
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-        //Siamo nel gruppo quindi:
-        // - il percorso "/" diventa "admin/"
-        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
-        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-        Route::resource('operators', AdminOperatorController::class);
-        Route::resource("operator-sponsorships", OperatorSponsorshipsController::class);
-        
-
 
     // Rotte per la gestione degli operatori
-    Route::get('/operators', [AdminOperatorController::class, 'index'])->name('operators.index');
-    Route::get('/operators/create', [AdminOperatorController::class, 'create'])->name('operators.create');
-    Route::post('/operators', [AdminOperatorController::class, 'store'])->name('operators.store');
-    Route::get('/operators/{operator}/edit', [AdminOperatorController::class, 'edit'])->name('operators.edit');
-    Route::put('/operators/{operator}', [AdminOperatorController::class, 'update'])->name('operators.update');
-    Route::delete('/operators/{operator}', [AdminOperatorController::class, 'destroy'])->name('operators.destroy');
-    Route::get('/operators/{operator}', [AdminOperatorController::class, 'show'])->name('operators.show');
-    Route::view('/error', 'admin.error')->name('error');
+    Route::resource('operators', AdminOperatorController::class);
+    Route::resource("operator-sponsorships", OperatorSponsorshipsController::class);
 
     Route::get("/my-messages/{operator_id}", [MyMessageController::class, "index"])->name("my-messages");
 
     Route::get("/my-review/{operator_id}", [MyReviewController::class, "index"])->name("my-reviews");
-
-
-
 });
 
 Route::get("/message", [MessageController::class, "index"]);
@@ -63,5 +55,3 @@ Route::get("/message", [MessageController::class, "index"]);
 Route::get("/review", [ReviewController::class, "index"]);
 
 require __DIR__ . '/auth.php';
-
-
