1b3f36261d06e7d0fda25d34acb25ed58a114f76
MarcoPercolla <percollamarco@virgilio.it>
2024-03-12 00:08:24 +0100

    seed minor

--- database/seeders/OperatorSpecializationTableSeeder.php
+++ database/seeders/OperatorSpecializationTableSeeder.php
@@ -17,58 +17,58 @@ class OperatorSpecializationTableSeeder extends Seeder
         $operator->specializations()->sync([1, 2]);
 
         $operator = Operator::find(2);
-        $operator->specializations()->sync([3, 4]);
+        $operator->specializations()->sync([12, 6]);
 
         $operator = Operator::find(3);
-        $operator->specializations()->sync([3, 4]);
+        $operator->specializations()->sync([2, 12]);
 
         $operator = Operator::find(4);
-        $operator->specializations()->sync([4, 5]);
+        $operator->specializations()->sync([3, 6]);
 
         $operator = Operator::find(5);
-        $operator->specializations()->sync([1, 14]);
-
-        $operator = Operator::find(6);
-        $operator->specializations()->sync([3, 15]);
-
-        $operator = Operator::find(7);
-        $operator->specializations()->sync([11]);
-
-        $operator = Operator::find(8);
-        $operator->specializations()->sync([1, 9]);
-
-        $operator = Operator::find(9);
-        $operator->specializations()->sync([10, 5]);
-
-        $operator = Operator::find(10);
-        $operator->specializations()->sync([4, 5]);
-
-        $operator = Operator::find(11);
-        $operator->specializations()->sync([3, 4]);
-
-        $operator = Operator::find(12);
         $operator->specializations()->sync([13, 14]);
 
+        $operator = Operator::find(6);
+        $operator->specializations()->sync([2, 4]);
+
+        $operator = Operator::find(7);
+        $operator->specializations()->sync([3, 7]);
+
+        $operator = Operator::find(8);
+        $operator->specializations()->sync([13, 15]);
+
+        $operator = Operator::find(9);
+        $operator->specializations()->sync([1, 8]);
+
+        $operator = Operator::find(10);
+        $operator->specializations()->sync([4, 15]);
+
+        $operator = Operator::find(11);
+        $operator->specializations()->sync([6, 7, 9]);
+
+        $operator = Operator::find(12);
+        $operator->specializations()->sync([13, 14, 12]);
+
         $operator = Operator::find(13);
-        $operator->specializations()->sync([1]);
+        $operator->specializations()->sync([5, 2, 12]);
 
         $operator = Operator::find(14);
-        $operator->specializations()->sync([2]);
+        $operator->specializations()->sync([3, 10]);
 
         $operator = Operator::find(15);
-        $operator->specializations()->sync([3, 4, 5]);
+        $operator->specializations()->sync([3, 2]);
 
         $operator = Operator::find(16);
-        $operator->specializations()->sync([15, 5]);
+        $operator->specializations()->sync([1, 6, 9]);
 
         $operator = Operator::find(17);
-        $operator->specializations()->sync([2, 6]);
+        $operator->specializations()->sync([14, 5]);
 
         $operator = Operator::find(18);
-        $operator->specializations()->sync([13]);
+        $operator->specializations()->sync([12, 11]);
 
         $operator = Operator::find(19);
-        $operator->specializations()->sync([14]);
+        $operator->specializations()->sync([11, 8]);
 
         $operator = Operator::find(20);
         $operator->specializations()->sync([4]);
