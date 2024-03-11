b2b90ed57d2e1c4525c86aa9a4cfd3e9496e702d
kristianfiliposki <krisplay@outlook.it>
2024-03-11 11:41:09 +0100

    linkedin e git hub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,52 @@
         </h1>
 
         <p class="col-md-8 fs-4">This a project,which was made by Kristian filiposki,Lorenzo Monteforte,Marco Percolla,Riccardo Rocco e Francesco Bonnanno</p>
+        <div>
+            <h3>Andate a vedere gli altri progetti dei nostri eroi!</h3>
+            <h4>Linkedin</h4>
+            <ul>
+                <li>
+                    <h3>
+                        Kristian Filiposki:
+                    </h3>
+                    <div>
+                        <a href="https://github.com/kristianfiliposki">GitHub</a>
+                        <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">Linkedin</a>
+
+                    </div>
+                </li>
+                <li>
+                    <h3>
+                       Lorenzo Monteforte
+                    </h3>
+                    <div>
+                        <a href="https://github.com/LorenzoMonteforte">GitHub</a>
+                        <a href="https://www.linkedin.com/in/lorenzo-monteforte-2846702b5/">Linkedin</a>
+
+                    </div>
+                </li>
+                <li>
+                    <h3>
+                        Marco Percolla
+                    </h3>
+                    <div>
+                        <a href="https://github.com/MarcoPercolla">GitHub</a>
+                        <a href="https://www.linkedin.com/in/marco-percolla-a358ab2b9/">Linkedin</a>
+
+                    </div>
+                </li>
+                <li>
+                    <h3>
+                       Riccardo Rocco
+                    </h3>
+                    <div>
+                        <a href="https://github.com/Riccardo-Rocco">GitHub</a>
+                        <a href="https://www.linkedin.com/in/riccardo-rocco-497251200/">Linkedin</a>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
     </div>
 </div>
 
