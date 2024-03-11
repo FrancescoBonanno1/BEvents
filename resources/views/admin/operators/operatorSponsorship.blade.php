552a8808f8bef9ea390f29ccbde96605b83d1e49
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-03-11 18:28:06 +0100

    Tolta possibilità di fare sponsorship

--- resources/views/admin/operators/operatorSponsorship.blade.php
+++ resources/views/admin/operators/operatorSponsorship.blade.php
@@ -13,7 +13,6 @@
         <div class="mb-3">
             <label for="sponsorship">Inserisci sponsorizzazione</label>
             <select id="sponsorship" name="sponsorship">
-                <option value="0">Nessuna sponsorizzazione</option>
                 @for($i=0; $i<sizeof($sponsorships); $i++)
                     <option value="{{ $sponsorships[$i]->id }}">{{ $sponsorships[$i]->duration }}</option>
                 @endfor    
